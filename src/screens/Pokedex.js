517e14ccd27b4ee8d58617f0b6815782d4390ec4
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-15 17:20:51 -0300

    Update Pokedex.js

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,20 +1,34 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState, useEffect } from "react";
-import { getPokemonApi } from "../api/pokemon";
+import { getPokemonApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+
   useEffect(() => {
     (async () => {
       await loadPokemons();
     })();
-    console.log("hola mundo");
   }, []);
 
   const loadPokemons = async () => {
     try {
       const response = await getPokemonApi();
-      console.log(response);
+
+      const pokemonArray = [];
+
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+
+        pokemonArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.type[0].type.name,
+        });
+      }
+
+      setPokemons(pokemonArray);
     } catch (error) {
       console.error(error);
     }
