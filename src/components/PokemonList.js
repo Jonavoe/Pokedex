d3dabf58ec1e2d0eb3217a77c5f3af1b34608d17
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-16 16:36:27 -0300

    Create PokemonList.js

---
+++ src/components/PokemonList.js
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { FlatList } from "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import PokemonCard from "./PokemonCard";
+
+export default function PokemonList(props) {
+  const { pokemons } = props;
+  return (
+    <FlatList
+      data={pokemons}
+      numColumns={2}
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(pokemon) => String(pokemon.id)}
+      renderItem={({ item }) => <PokemonCard pokemon={item} />}
+      contentContainerStyle={styles.flatListContentContainer}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  flatListContentContainer: {
+    paddingHorizontal: 5,
+  },
+});
