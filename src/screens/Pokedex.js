9b4efc154260c77c1e8ecd370a7194359cc29c76
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-15 21:14:45 -0300

    Update Pokedex.js

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,84 +1,142 @@
-import { View, Text, Image, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from "react-native";
 import { getPokemonApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
-import { ScrollView } from "react-native-gesture-handler";
+
+const typeColors = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+};
 
 export default function Pokedex() {
+  const [apiPokemons, setApiPokemons] = useState([]);
   const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    (async () => {
-      await loadPokemons();
-    })();
-  }, []);
+    loadApiPokemons();
+  }, [page]);
 
-  const loadPokemons = async () => {
-    try {
-      const response = await getPokemonApi();
+  useEffect(() => {
+    loadPokemons(apiPokemons);
+  }, [apiPokemons]);
 
-      const pokemonArray = [];
+  const loadApiPokemons = async () => {
+    const response = await getPokemonApi(page);
+    setApiPokemons(response.results);
+  };
 
-      for await (const pokemon of response.results) {
+  const loadPokemons = async (apiPokemons) => {
+    setPokemons([]);
+    const newPokemons = await Promise.all(
+      apiPokemons.map(async (pokemon) => {
         const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
-
-        pokemonArray.push({
+        const newPokemon = {
           id: pokemonDetails.id,
           name: pokemonDetails.name,
           type: pokemonDetails.types[0].type.name,
           order: pokemonDetails.order,
           imagen:
             pokemonDetails.sprites.other["official-artwork"].front_default,
-        });
-      }
+        };
+        return newPokemon;
+      })
+    );
 
-      setPokemons((prevPokemons) => [...prevPokemons, ...pokemonArray]);
-    } catch (error) {
-      console.error(error);
-    }
+    const uniquePokemons = newPokemons.filter(
+      (newPokemon) => !pokemons.some((p) => p.id === newPokemon.id)
+    );
+
+    setPokemons((prevPokemons) => [...prevPokemons, ...uniquePokemons]);
   };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollContainer}>
+    <ScrollView style={styles.mainContainer}>
+      <View style={styles.pokemonContainer}>
         {pokemons.map((pokemon, index) => (
-          <View key={index} style={styles.pokemonContainer}>
+          <View
+            key={index}
+            style={[
+              styles.pokemonItem,
+              { backgroundColor: typeColors[pokemon.type] || "#CCCCCC" },
+            ]}
+          >
             <Image
               source={{ uri: pokemon.imagen }}
               style={styles.pokemonImage}
             />
-            <View style={styles.pokemonText}>
+            <View>
               <Text>{pokemon.name}</Text>
               <Text>{pokemon.type}</Text>
               <Text>{pokemon.order}</Text>
             </View>
           </View>
         ))}
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+      <View style={styles.paginationContainer}>
+        <Button
+          title="Previous Page"
+          onPress={() => setPage(Math.max(1, page - 1))}
+          disabled={page === 1}
+        />
+        <Text>{page}</Text>
+        <Button title="Next Page" onPress={() => setPage(page + 1)} />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  scrollContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
+    borderWidth: 2,
+    borderColor: "black",
   },
   pokemonContainer: {
     flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
-  pokemonText: {
-    marginLeft: 2,
+  pokemonItem: {
+    width: 180,
+    height: 180,
+    margin: 4,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "orange",
+    alignItems: "center",
+    justifyContent: "center",
   },
   pokemonImage: {
     width: 100,
-    aspectRatio: 1,
+    height: 100,
+    resizeMode: "contain",
+  },
+  paginationContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
   },
 });
