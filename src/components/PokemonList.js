05eb8798b0bda6407d2e705a4062bbea987ffa02
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-17 18:05:33 -0300

    Update PokemonList.js

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,9 +1,10 @@
 import { View, ActivityIndicator, FlatList, StyleSheet } from "react-native";
 import React from "react";
 import PokemonCard from "./PokemonCard";
+import { Platform } from "react-native";
 
 export default function PokemonList(props) {
-  const { pokemons, loadPokemons } = props;
+  const { pokemons, loadPokemons, isNext } = props;
 
   const loadMore = () => {
     loadPokemons();
@@ -17,14 +18,16 @@ export default function PokemonList(props) {
       keyExtractor={(pokemon) => String(pokemon.id)}
       renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
-      onEndReached={loadMore}
+      onEndReached={isNext && loadMore}
       onEndReachedThreshold={0.1}
       ListFooterComponent={
-        <ActivityIndicator
-          size="large"
-          style={styles.spinner}
-          color="#AEAEAE"
-        />
+        isNext && (
+          <ActivityIndicator
+            size="large"
+            style={styles.spinner}
+            color="#AEAEAE"
+          />
+        )
       }
     />
   );
@@ -33,9 +36,10 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
+    marginTop: Platform.OS === "android" ? 30 : 0,
   },
   spinner: {
     marginTop: 20,
-    marginBottom: 60,
+    marginBottom: Platform.OS === "android" ? 90 : 60,
   },
 });
