a08ab0569efec07aef86168ccb7d879d7df982b6
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-18 17:47:46 -0300

    Update Pokemon.js

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { Text, ScrollView } from "react-native";
 import { getPokemonDetailsApi } from "../api/pokemon";
+import Header from "../components/Pokemon/Header";
+import {} from "react-native";
+import Type from "../components/Pokemon/Type";
+import Stats from "../components/Pokemon/Stats";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 export default function Pokemon(props) {
   const {
@@ -11,19 +16,46 @@ export default function Pokemon(props) {
   const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
+    navigation.setOptions({
+      headerRigth: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{
+            marginLeft: 20,
+            marginBottom: 5,
+          }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
+
+  useEffect(() => {
     (async () => {
       try {
         const response = await getPokemonDetailsApi(params.id);
-        console.log(response);
+        setPokemon(response);
       } catch (error) {
         navigation.goBack();
       }
     })();
   }, [params]);
 
+  if (!pokemon) return null;
+
   return (
-    <View>
-      <Text>Estamos en un POKEMON</Text>
-    </View>
+    <ScrollView>
+      <Header
+        name={pokemon.name}
+        order={pokemon.order}
+        image={pokemon.sprites.other["home"].front_default}
+        type={pokemon.types[0].type.name}
+      />
+      <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
+    </ScrollView>
   );
 }
